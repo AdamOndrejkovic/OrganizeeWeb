3af71a14404c808bb6e5dcb8953a1d505ed0aedd
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2022-04-30 18:25:27 +0200

    models and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TodoOverviewComponent } from './todo-overview/todo-overview.component';
+import { AddEditComponent } from './add-edit/add-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    TodoOverviewComponent,
+    AddEditComponent
   ],
   imports: [
     BrowserModule
