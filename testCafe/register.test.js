742a0aeb25d035317fab399130555f19c50e3420
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2022-05-24 16:43:56 +0200

    Fix

--- testCafe/register.test.js
+++ testCafe/register.test.js
@@ -6,14 +6,11 @@ fixture`Register testing`
 test('Register name', async t => {
     await t
         .typeText('#name', 'John Smith')
-
-        // Use the assertion to check if the actual header text is equal to the expected one
         .expect(Selector('#name').value).eql('John Smith');
 });
 
 test('Register password', async t => {
     await t
         .typeText('#password', 'Top secret password')
-        // Use the assertion to check if the actual header text is equal to the expected one
         .expect(Selector('#password').value).eql('Top secret password');
 });
